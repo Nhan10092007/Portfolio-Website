c6f57d5d7e89633a3b7127d3cd1403cd7c693d48
LuuThienNhan <nhanthienluu1009@gmail.com>
2026-05-25 21:16:47 +0700

    Dark theme update

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const projectspage = document.querySelector(".projects-page");
 const resumepage = document.querySelector(".resume-page");
 const contactpage = document.querySelector(".contact-page");
 
-
 const homebtn = document.getElementById("homebtn");
 const projectsbtn = document.getElementById("projectsbtn");
 const resumebtn = document.getElementById("resumebtn"); 
@@ -39,5 +38,25 @@ function switchSolve(curBtn){
     }
     else if(curBtn == contactbtn){
         contactpage.classList.add("active")
+    }
+}
+
+const themeicon = document.getElementById("theme-icon")
+const theme = document.querySelector(".theme")
+const changethemebtn = document.querySelector(".change-theme")
+changethemebtn.addEventListener("click", ()=> changeTheme())
+function changeTheme(){
+    if(theme.classList.contains("dark-mode")){
+        theme.classList.remove("dark-mode")
+    }
+    else{
+        theme.classList.add("dark-mode");
+    }
+    let curSrc = themeicon.getAttribute("src")
+    if(curSrc === "Image/moon.png"){
+        themeicon.src = "Image/sun.png"
+    }
+    else{
+        themeicon.src = "Image/moon.png"
     }
 }
